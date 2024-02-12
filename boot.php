f216af19aabbdb9533bf0682fb0ff455587ca12e
Thomas Skerbis <thomas.skerbis@mac.com>
2024-02-12 22:16:54 +0100

    added: rex_yform_value_mediafile::isMediaInUse

--- boot.php
+++ boot.php
@@ -2,3 +2,5 @@
 rex_extension::register('PACKAGES_INCLUDED', function (rex_extension_point $ep) {
     rex_yform::addTemplatePath($this->getPath('ytemplates'));
 });
+
+\rex_extension::register('MEDIA_IS_IN_USE', 'rex_yform_value_mediafile::isMediaInUse');
