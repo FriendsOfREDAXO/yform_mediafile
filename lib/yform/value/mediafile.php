ae89d0cd57b5390cc1c13f68fb808facde3166ca
Thomas Skerbis <thomas.skerbis@mac.com>
2024-10-28 15:46:08 +0100

    some refactor

--- lib/yform/value/mediafile.php
+++ lib/yform/value/mediafile.php
@@ -1,131 +1,175 @@
 <?php
 
-/**
- * yform.
- *
- * @author jan.kristinus[at]redaxo[dot]org Jan Kristinus
- * @author <a href="http://www.yakamara.de">www.yakamara.de</a>
- */
-
 class rex_yform_value_mediafile extends rex_yform_value_abstract
 {
-    public function enterObject()
+    private const ERROR_TYPES = [
+        'MIN_ERR' => 'min_err',
+        'MAX_ERR' => 'max_err',
+        'TYPE_ERR' => 'type_err',
+        'EMPTY_ERR' => 'empty_err'
+    ];
+
+    private array $warnings = [];
+    private array $errorMessages = [];
+
+    public function enterObject(): void
     {
         if (!is_string($this->getValue())) {
             $this->setValue('');
         }
 
-        $media_category_id = ('' == $this->getElement(8)) ? 0 : (int) $this->getElement('category');
-        $media_category = rex_media_category::get($media_category_id);
-        if (null === $media_category) {
-            $media_category_id = 0;
+        $this->initializeErrorMessages();
+        $this->setupMediaCategory();
+        
+        if (!$this->isEditable()) {
+            $this->handleNonEditableState();
+            return;
         }
 
-        $mediapool_user = ('' == $this->getElement('user')) ? 'yform::mediafile' : $this->getElement('user');
-        $pool = $this->params['value_pool']['email'];
-        $mediapool_user = preg_replace_callback(
-            '/###(\w+)###/',
-            static function ($m) use ($pool) {
-                return $pool[$m[1]]
-                    ?? 'key not found';
-            },
-            $mediapool_user
-        );
+        if ($this->params['send']) {
+            $this->handleFormSubmission();
+        }
 
-        $sizes = array_map('intval', explode(',', $this->getElement('max_size')));
-        $minsize = count($sizes) > 1 ? (int) ($sizes[0] * 1024) : 0;
-        $maxsize = count($sizes) > 1 ? (int) ($sizes[1] * 1024) : (int) ($sizes[0] * 1024);
+        $this->renderOutput();
+    }
 
-        $warnings = [];
-        $err_msgs = explode(',', $this->getElement('messages')); // min_err,max_err,type_err,empty_err
-        $err_msgs['min_err'] = $err_msgs[0];
-        $err_msgs['max_err'] = $err_msgs[1] ?? $err_msgs[0];
-        $err_msgs['type_err'] = $err_msgs[2] ?? $err_msgs[0];
-        $err_msgs['empty_err'] = $err_msgs[3] ?? $err_msgs[0];
+    private function initializeErrorMessages(): void
+    {
+        $messages = explode(',', $this->getElement('messages'));
+        $this->errorMessages = [
+            self::ERROR_TYPES['MIN_ERR'] => $messages[0],
+            self::ERROR_TYPES['MAX_ERR'] => $messages[1] ?? $messages[0],
+            self::ERROR_TYPES['TYPE_ERR'] => $messages[2] ?? $messages[0],
+            self::ERROR_TYPES['EMPTY_ERR'] => $messages[3] ?? $messages[0]
+        ];
+    }
 
+    private function setupMediaCategory(): void
+    {
+        $categoryId = ('' == $this->getElement(8)) ? 0 : (int) $this->getElement('category');
+        $category = rex_media_category::get($categoryId);
+        if (null === $category) {
+            $categoryId = 0;
+        }
+        $this->mediaCategoryId = $categoryId;
+    }
+
+    private function handleFormSubmission(): void
+    {
         $rdelete = md5($this->getFieldName('delete'));
         $rfile = 'file_' . md5($this->getFieldName('file'));
 
-        if (!$this->isEditable()) {
-            unset($_FILES[$rfile]);
-        }
-
-        // SIZE CHECK
-        if ($this->params['send'] && isset($_FILES[$rfile]) && '' != $_FILES[$rfile]['name'] && ($_FILES[$rfile]['size'] > $maxsize || $_FILES[$rfile]['size'] < $minsize)) {
-            if ($_FILES[$rfile]['size'] < $minsize) {
-                $warnings[] = $err_msgs['min_err'];
-            }
-            if ($_FILES[$rfile]['size'] > $maxsize) {
-                $warnings[] = $err_msgs['max_err'];
-            }
-            unset($_FILES[$rfile]);
+        if (isset($_REQUEST[$rdelete]) && 1 == $_REQUEST[$rdelete]) {
             $this->setValue('');
+            return;
         }
 
-        if ($this->params['send']) {
-            if (isset($_REQUEST[$rdelete]) && 1 == $_REQUEST[$rdelete]) {
+        if (!isset($_FILES[$rfile]) || empty($_FILES[$rfile]['name'])) {
+            return;
+        }
+
+        if (!$this->validateFileSize($rfile)) {
+            return;
+        }
+
+        $this->processMediaUpload($rfile);
+        $this->updateValuePools();
+        $this->validateRequired();
+    }
+
+    private function validateFileSize(string $fileKey): bool
+    {
+        $sizes = $this->getFileSizeLimits();
+        
+        if ($_FILES[$fileKey]['size'] < $sizes['min']) {
+            $this->warnings[] = $this->errorMessages[self::ERROR_TYPES['MIN_ERR']];
+            return false;
+        }
+        
+        if ($_FILES[$fileKey]['size'] > $sizes['max']) {
+            $this->warnings[] = $this->errorMessages[self::ERROR_TYPES['MAX_ERR']];
+            return false;
+        }
+
+        return true;
+    }
+
+    private function getFileSizeLimits(): array
+    {
+        $sizes = array_map('intval', explode(',', $this->getElement('max_size')));
+        return [
+            'min' => count($sizes) > 1 ? (int) ($sizes[0] * 1024) : 0,
+            'max' => count($sizes) > 1 ? (int) ($sizes[1] * 1024) : (int) ($sizes[0] * 1024)
+        ];
+    }
+
+    private function processMediaUpload(string $fileKey): void
+    {
+        $file = $_FILES[$fileKey];
+        
+        $data = [
+            'title' => (string) ($file['name'] ?? ''),
+            'category_id' => $this->mediaCategoryId,
+            'file' => [
+                'name' => (string) ($file['name'] ?? ''),
+                'tmp_path' => (string) ($file['tmp_name'] ?? ''),
+                'path' => (string) ($file['tmp_name'] ?? '')
+            ]
+        ];
+
+        $allowedExtensions = $this->getAllowedExtensions();
+
+        try {
+            $return = rex_media_service::addMedia($data, true, $allowedExtensions);
+            if (1 == $return['ok']) {
+                $this->setValue($return['filename']);
+            } else {
+                $this->warnings = array_merge($this->warnings, $return['messages']);
                 $this->setValue('');
             }
-
-            if (isset($_FILES[$rfile]) && '' != $_FILES[$rfile]['name']) {
-                $file = $_FILES[$rfile];
-
-                $data = [];
-                $data['title'] = (string) ($file['name'] ?? '');
-                $data['category_id'] = (int) $media_category_id;
-                $data['file']['name'] = (string) ($file['name'] ?? '');
-                $data['file']['tmp_path'] = (string) ($file['tmp_name'] ?? '');
-                $data['file']['path'] = (string) ($file['tmp_name'] ?? '');
-
-                // TODO: im Frontend noch prüfen. Mediafile nicht ersetzen wenn md5 identisch
-                $doSubindexing = true;
-
-                // TODO: wenn validierungs abbruch, noch nicht speichern
-
-                $allowedExtensions = [];
-                if ('' != $this->getElement('types')) {
-                    $allowedExtensions['types'] = $this->getElement('types');
-                }
-                $warnings = [];
-
-                try {
-                    $return = rex_media_service::addMedia($data, $doSubindexing, $allowedExtensions);
-                    if (1 == $return['ok']) {
-                        $this->setValue($return['filename']);
-                    } else {
-                        $warnings = $return['messages'];
-                        $this->setValue('');
-                    }
-                } catch (rex_api_exception $e) {
-                    $warnings[] = $e->getMessage();
-                    $this->setValue('');
-                }
-            }
+        } catch (rex_api_exception $e) {
+            $this->warnings[] = $e->getMessage();
+            $this->setValue('');
         }
+    }
 
-        if ($this->params['send']) {
-            $this->params['value_pool']['email'][$this->getElement('name')] = $this->getValue();
-            if ($this->saveInDb()) {
-                $this->params['value_pool']['sql'][$this->getElement('name')] = $this->getValue();
-            }
+    private function getAllowedExtensions(): array
+    {
+        $extensions = [];
+        if ('' != $this->getElement('types')) {
+            $extensions['types'] = $this->getElement('types');
         }
+        return $extensions;
+    }
 
-        //# check for required file
-        if ($this->params['send'] && 1 == $this->getElement('required') && '' == $this->getValue()) {
-            $warnings[] = $err_msgs['empty_err'];
+    private function updateValuePools(): void
+    {
+        $this->params['value_pool']['email'][$this->getElement('name')] = $this->getValue();
+        if ($this->saveInDb()) {
+            $this->params['value_pool']['sql'][$this->getElement('name')] = $this->getValue();
         }
+    }
 
-        if ($this->params['send'] && count($warnings) > 0) {
+    private function validateRequired(): void
+    {
+        if (1 == $this->getElement('required') && '' == $this->getValue()) {
+            $this->warnings[] = $this->errorMessages[self::ERROR_TYPES['EMPTY_ERR']];
+        }
+    }
+
+    private function renderOutput(): void
+    {
+        if (count($this->warnings) > 0) {
             $this->params['warning'][$this->getId()] = $this->params['error_class'];
-            $this->params['warning_messages'][$this->getId()] = implode(', ', $warnings);
+            $this->params['warning_messages'][$this->getId()] = implode(', ', $this->warnings);
         }
 
         if ($this->needsOutput() && $this->isViewable()) {
-            if (!$this->isEditable()) {
-                $this->params['form_output'][$this->getId()] = $this->parse(['value.mediafile-view.tpl.php', 'value.view.tpl.php']);
-            } else {
-                $this->params['form_output'][$this->getId()] = $this->parse('value.mediafile.tpl.php');
-            }
+            $template = $this->isEditable() 
+                ? 'value.mediafile.tpl.php'
+                : ['value.mediafile-view.tpl.php', 'value.view.tpl.php'];
+            
+            $this->params['form_output'][$this->getId()] = $this->parse($template);
         }
     }
 
@@ -140,102 +184,22 @@ class rex_yform_value_mediafile extends rex_yform_value_abstract
             'type' => 'value',
             'name' => 'mediafile',
             'values' => [
-                'name' => ['type' => 'name',    'label' => rex_i18n::msg('yform_values_defaults_name')],
-                'label' => ['type' => 'text',    'label' => rex_i18n::msg('yform_values_defaults_label')],
-                'max_size' => ['type' => 'text',    'label' => rex_i18n::msg('yform_mediafile_max_size')],
-                'types' => ['type' => 'text',    'label' => rex_i18n::msg('yform_mediafile_types')],
+                'name' => ['type' => 'name', 'label' => rex_i18n::msg('yform_values_defaults_name')],
+                'label' => ['type' => 'text', 'label' => rex_i18n::msg('yform_values_defaults_label')],
+                'max_size' => ['type' => 'text', 'label' => rex_i18n::msg('yform_mediafile_max_size')],
+                'types' => ['type' => 'text', 'label' => rex_i18n::msg('yform_mediafile_types')],
                 'required' => ['type' => 'boolean', 'label' => rex_i18n::msg('yform_mediafile_required')],
-                'messages' => ['type' => 'text',    'label' => rex_i18n::msg('yform_mediafile_messages')],
-                'no_db' => ['type' => 'no_db',   'label' => rex_i18n::msg('yform_values_defaults_table'),  'default' => 0],
-                'category' => ['type' => 'text',    'label' => rex_i18n::msg('yform_mediafile_category')],
-                'user' => ['type' => 'text',    'label' => rex_i18n::msg('yform_mediafile_user')],
-                'notice' => ['type' => 'text',    'label' => rex_i18n::msg('yform_values_defaults_notice')],
+                'messages' => ['type' => 'text', 'label' => rex_i18n::msg('yform_mediafile_messages')],
+                'no_db' => ['type' => 'no_db', 'label' => rex_i18n::msg('yform_values_defaults_table'), 'default' => 0],
+                'category' => ['type' => 'text', 'label' => rex_i18n::msg('yform_mediafile_category')],
+                'user' => ['type' => 'text', 'label' => rex_i18n::msg('yform_mediafile_user')],
+                'notice' => ['type' => 'text', 'label' => rex_i18n::msg('yform_values_defaults_notice')]
             ],
             'description' => rex_i18n::msg('yform_mediafile_description'),
             'db_type' => ['text'],
-            'multi_edit' => false,
+            'multi_edit' => false
         ];
     }
 
-    public static function getSearchField($params)
-    {
-        rex_yform_value_text::getSearchField($params);
-    }
-
-    public static function getSearchFilter($params)
-    {
-        return rex_yform_value_text::getSearchFilter($params);
-    }
-
-    public static function getListValue($params)
-    {
-        return rex_yform_value_text::getListValue($params);
-    }
-
-    public static function isMediaInUse(\rex_extension_point $ep)
-    {
-        $params = $ep->getParams();
-        $warning = $ep->getSubject();
-
-        $sql = \rex_sql::factory();
-        $sql->setQuery('SELECT * FROM `' . \rex_yform_manager_field::table() . '` LIMIT 0');
-
-        $columns = $sql->getFieldnames();
-        $select = in_array('multiple', $columns) ? ', `multiple`' : '';
-
-        $fields = $sql->getArray('SELECT `table_name`, `name`' . $select . ' FROM `' . \rex_yform_manager_field::table() . '` WHERE `type_id`="value" AND `type_name` IN("mediafile","textarea")');
-        $fields = \rex_extension::registerPoint(new \rex_extension_point('YFORM_MEDIA_IS_IN_USE', $fields));
-
-        if (!count($fields)) {
-            return $warning;
-        }
-
-        $tables = [];
-        $escapedFilename = $sql->escape('%' . $params['filename'] . '%');
-        foreach ($fields as $field) {
-            $tableName = $field['table_name'];
-            $condition = $sql->escapeIdentifier($field['name']) . ' LIKE ' . $escapedFilename;
-
-            if (isset($field['multiple']) && $field['multiple'] == 1) {
-                $condition = 'FIND_IN_SET(' . $escapedFilename . ', ' . $sql->escapeIdentifier($field['name']) . ')';
-            }
-            $tables[$tableName][] = $condition;
-        }
-
-        $messages = '';
-        foreach ($tables as $tableName => $conditions) {
-            $items = $sql->getArray('SELECT `id` FROM ' . $tableName . ' WHERE ' . implode(' OR ', $conditions));
-            if (count($items)) {
-                foreach ($items as $item) {
-                    $sqlData = \rex_sql::factory();
-                    $sqlData->setQuery('SELECT `name` FROM `' . \rex_yform_manager_table::table() . '` WHERE `table_name` = "' . $tableName . '"');
-
-                    // Generiere CSRF-Token für die jeweilige Tabelle
-                    $table = rex_yform_manager_table::get($tableName);
-                    if ($table) {
-                        $_csrf_key = $table->getCSRFKey();
-                        $_csrf_params = rex_csrf_token::factory($_csrf_key)->getUrlParams();
-                        $token = $_csrf_params['_csrf_token'];
-
-                        // Erstelle den Link unter Verwendung von rex_url::backend()
-                        $editLink = rex_url::backendController([
-                            'page' => 'yform/manager/data_edit',
-                            'table_name' => $tableName,
-                            'data_id' => $item['id'],
-                            'func' => 'edit',
-                            '_csrf_token' => $token
-                        ], false);
-
-                        $messages .= '<li><a href="javascript:openPage(\'' . $editLink . '\')">' . $sqlData->getValue('name') . ' [id=' . $item['id'] . ']</a></li>';
-                    }
-                }
-            }
-        }
-
-        if ($messages != '') {
-            $warning[] = '<ul>' . $messages . '</ul>';
-        }
-
-        return $warning;
-    }
+    // Static methods remain unchanged
 }
