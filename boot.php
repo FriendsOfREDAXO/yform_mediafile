08bcda850e29637b4c71c0cba397a053f7869c19
Thomas Skerbis <thomas.skerbis@mac.com>
2024-10-28 16:00:01 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,7 +1,8 @@
-// Erste Registrierung ist korrekt, da sie eine anonyme Funktion verwendet
+// Boot.php
 rex_extension::register('PACKAGES_INCLUDED', function (rex_extension_point $ep) {
-    rex_yform::addTemplatePath($this->getPath('ytemplates'));
+    // Statt $this das Addon-Objekt verwenden
+    rex_yform::addTemplatePath(rex_addon::get('yform_mediafile')->getPath('ytemplates'));
 });
 
-// Zweite Registrierung muss als Callable übergeben werden
+// Statische Methode als Array-Callable registrieren
 rex_extension::register('MEDIA_IS_IN_USE', [rex_yform_value_mediafile::class, 'isMediaInUse']);
