4d147210377d03d9e50275f098b061410fa639e9
Thomas Skerbis <thomas.skerbis@mac.com>
2024-10-28 16:03:56 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,9 +1,7 @@
-<?php 
+<?php
 // Boot.php
-rex_extension::register('PACKAGES_INCLUDED', function (rex_extension_point $ep) {
-    // Statt $this das Addon-Objekt verwenden
-    rex_yform::addTemplatePath(rex_addon::get('yform_mediafile')->getPath('ytemplates'));
+$addon = rex_addon::get('yform_mediafile');
+rex_extension::register('PACKAGES_INCLUDED', function (rex_extension_point $ep) use ($addon) {
+    rex_yform::addTemplatePath($addon->getPath('ytemplates'));
 });
 
-// Statische Methode als Array-Callable registrieren
-rex_extension::register('MEDIA_IS_IN_USE', [rex_yform_value_mediafile::class, 'isMediaInUse']);
